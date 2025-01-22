8c92e97ce9f1c10f114d7854fdaf15476156b991
Dawid Mostowski <dawid.mostowski06@proton.me>
2025-01-22 22:37:03 +0100

    Update szachy.php

--- Klasa 4/Zadanie egzaminacyjne/2/szachy.php
+++ Klasa 4/Zadanie egzaminacyjne/2/szachy.php
@@ -8,16 +8,24 @@
 </head>
 <body>
     <section class="naglowek">
-
+        <h2>Koło szachowe gambit piona</h2>
     </section>
     <section class="lewy">
-
+        <h4>Polecane linki</h4>
+            <li><a href="./kwerendy/kw1.png">kwerenda1</a></li>
+            <li><a href="./kwerendy/kw2.png"">kwerenda2</a></li>
+            <li><a href="./kwerendy/kw3.png"">kwerenda3</a></li>
+            <li><a href="./kwerendy/kw4.png"">kwerenda3</a></li>
+        <img src="./logo.png" alt="Logo koła">
     </section>
     <section class="prawy">
-
+        <h3>Najlepsi gracze naszego koła</h3>
+        <!-- tabela
+        formularz -->
+        <p>Legenda: AM - Absolutny Mistrz, SM - Szkolny Mistrz, PM - Mistrz Poziomu, KM - Mistrz Klasowy</p>
     </section>
     <section class="stopka">
-        
+        <p>Stronę wykonał: 11</p>
     </section>
 </body>
 </html>
