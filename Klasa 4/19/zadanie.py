6d0bfb3e18f6880d9e506cf314eca03e5961ba3c
Dawid Mostowski <dawid.mostowski06@proton.me>
2025-01-24 12:15:10 +0100

    update

---
+++ Klasa 4/19/zadanie.py
@@ -0,0 +1,7 @@
+liczba1 = float(input("Podaj pierwszą liczbę: "))
+
+liczba2 = float(input("Podaj drugą liczbę: "))
+
+suma = liczba1 + liczba2
+
+print(f"Suma liczb {liczba1} i {liczba2} wynosi: {suma}")
