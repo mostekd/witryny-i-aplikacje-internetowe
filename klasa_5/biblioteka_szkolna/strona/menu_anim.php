2035916b5ea7dae6340777c88bb87752839ce638
Dawid Mostowski <dawid.mostowski06@proton.me>
2025-10-17 12:08:58 +0200

    update

--- klasa_5/biblioteka_szkolna/strona/menu_anim.php
+++
@@ -1,6 +0,0 @@
-<div class="menu">
-  <a href="index.php">Strona główna</a>
-  <a href="ksiegozbior.php">Wyszukiwanie książek</a>
-  <a href="ksiega_gosci.php">Księga gości</a>
-  <a href="kontakt.php">Kontakt</a>
-</div>
